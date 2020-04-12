6ca0a9ce1a200c75eb8212363e2fe2975d5697a6
atikash <32333519+atikash@users.noreply.github.com>
2020-04-12 18:40:06 +0530

    Add files via upload
    Initial draft

---
+++ src/main/java/com/atikash/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.atikash.rest.webservices.restfulwebservices.bean;
+
+public class HelloWorldBean {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HelloWorldBean(String hello_world) {
+    this.message=hello_world;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
